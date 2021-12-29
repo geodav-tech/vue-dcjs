d93871cd4fc79674d13bede479ded0c675349b83
Shawn Pacarar <spacarar@hotmail.com>
2021-12-29 10:36:44 -0700

    fix bundle configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import DcPlugin from '@/plugins/dc.plugin'
 import DcRowChart from '@/components/dc-row-chart'
 
-export default {
+const VueDc = {
   install (Vue, options) {
     // register $dc, $d3, $crossfilter
     Vue.use(DcPlugin, options)
@@ -9,4 +9,9 @@ export default {
     // register out of the box components
     Vue.component('dc-row-chart', DcRowChart)
   }
+}
+
+// fix warning prefer_named_exports by naming this export
+export {
+  VueDc
 }
