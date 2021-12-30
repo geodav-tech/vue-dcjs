814e03a0063ee1a76e651982a95787e30e021c46
Shawn Pacarar <spacarar@hotmail.com>
2021-12-30 10:34:39 -0700

    missing part from ee8ec3065b7ffa2e1acb7167abab11639bea09b9

--- src/plugins/dc.plugin.js
+++ src/plugins/dc.plugin.js
@@ -3,15 +3,16 @@ import * as d3 from 'd3'
 import crossfilter from 'crossfilter2'
 import { AxisChart } from './axis-chart.class'
 
+dc.AxisChart = AxisChart
+
 /**
  * import DcPlugin from 'this.file'
  * Vue.use(DcPlugin)
  * 
  * binds dc, d3, and crossfilter to vue via $dc, $d3, $crossfilter
  */
-export default {
+const DcPlugin = {
   install (Vue, options) {
-    dc.AxisChart = AxisChart
     if (options?.defaultColors) {
       dc.config.defaultColors = options.defaultColors
     }
@@ -20,4 +21,12 @@ export default {
     Vue.prototype.$d3 = d3
     Vue.prototype.$crossfilter = crossfilter
   }
+}
+
+export default DcPlugin
+export {
+  DcPlugin,
+  dc,
+  d3,
+  crossfilter
 }
