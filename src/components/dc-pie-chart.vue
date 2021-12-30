0daf7995635d5e8ba5d356ca2ec0cd78c5d0f22b
Shawn Pacarar <spacarar@hotmail.com>
2021-12-30 12:05:06 -0700

    pie chart with better label position, and label wrapping options

---
+++ src/components/dc-pie-chart.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="dc-chart-container">
+    <div :id="`chart-${_uid}`" class="dc-chart dc-pie-chart"></div>
+  </div>
+</template>
+
+<script>
+import BaseChartMixin from '../mixins/base-chart.mixin.vue'
+import DimensionMixin from '../mixins/dimension.mixin.vue'
+import GroupMixin from '../mixins/group.mixin.vue'
+import { constrain } from '../dc-utils.js'
+
+export default {
+  name: 'DcPieChart',
+  mixins: [BaseChartMixin, DimensionMixin, GroupMixin],
+  defaultOptions: {
+    minRadius: 0,
+    maxRadius: Infinity,
+    innerRadius: 0,
+    slicesCap: 10,
+    othersLabel: 'others',
+    useImprovedLabels: true,
+    labelWrapSeparator: ';|;' // if using improved labels you can separate key and value by this separator
+  },
+  methods: {
+    createChart () {
+      this.$options.dimension = this.createDimension()
+      const group = this.createGroup(this.$options.dimension)
+      this.chart = new this.$dc.PieChart(`#chart-${this._uid}`)
+        .dimension(this.$options.dimension)
+        .group(group)
+        .externalRadiusPadding(12)
+      this.$super(BaseChartMixin).createChart()
+
+      let radius = this.getRadius()
+      this.chart.radius(radius)
+      this.chart.width(radius)
+      this.chart.height(radius)
+      let { innerRadius, slicesCap, othersLabel, useImprovedLabels } = this.computedOptions
+      if (slicesCap) {
+        this.chart.slicesCap(slicesCap)
+      }
+      if (othersLabel) {
+        this.chart.othersLabel(othersLabel)
+      }
+      if (innerRadius) {
+        // either exact number or 'X%' e.g. '50%'
+        let ir = typeof innerRadius === 'number' ? innerRadius : radius * (parseInt(innerRadius) || 0) / 100
+        this.chart.innerRadius(ir)
+      }
+
+      if (useImprovedLabels) {
+        const buildArc = () => {
+          const maxRadius = Math.min(this.chart.width(), this.chart.height()) / 1.8
+          const radius = Math.min(this.chart.radius(), maxRadius)
+          return this.$d3.arc().outerRadius(radius).innerRadius(this.chart.innerRadius())
+        }
+
+        const labelPosition = (d, arc) => {
+          const centroid = arc.centroid(d)
+          if (centroid.some(coord => isNaN(coord))) {
+            return [0, 0]
+          } else {
+            return centroid
+          }
+        }
+
+        const wrap = (text) => {
+          // take text `key;|;value` and transform to `key` and `(value)` on two lines
+          const d3 = this.$d3
+          const labelWrapSeparator = this.computedOptions.labelWrapSeparator
+          text.each(function () {
+            const el = d3.select(this)
+            const words = el.text().split(labelWrapSeparator)
+            if (!words || words.length < 2) {
+              return // no need to wrap. not enough words
+            }
+
+            const tString = el.attr('transform')
+            const x = parseFloat(tString.slice(tString.indexOf('translate(') + 10, tString.indexOf(',') - 1))
+            const y = parseFloat(tString.slice(tString.indexOf(',') + 1, tString.indexOf(')')))
+            let tspan = el.text(null).append('tspan').attr('transform', `translate(${x}, ${y})`).attr('x', 0).attr('y', 0)
+            tspan.text(words[0])
+            tspan = el.append('tspan').attr('transform', `translate(${x}, ${y})`).attr('x', 0).attr('y', 14)
+            tspan.text(`(${words[1].replace(/[^0-9\-.]/g, '')})`)
+          })
+        }
+
+        const handleLabelTransition = () => {
+          this.chart.selectAll('text.pie-label').attr('transform', (d, i) => {
+            const arc = buildArc(this.chart)
+            const xy = labelPosition(d, arc)
+            let mult = (i + 1) / 6 + 0.3
+            if (xy[1] < 0) {
+              mult = 1
+            }
+            return `translate(${xy[0]}, ${xy[1] * mult})`
+          })
+          this.chart.selectAll('.pie-label-group text.pie-label').transition().call(wrap)
+        }
+
+        this.chart.on('renderlet', chart => {
+          const radius = this.getRadius()
+          if (this.chart.radius() !== radius) {
+            this.chart.height(radius)
+            this.chart.width(radius)
+            this.chart.redraw()
+          }
+          handleLabelTransition()
+        })
+        this.chart.on('pretransition', handleLabelTransition)
+      }
+    },
+    renderChart () {
+      return new Promise(resolve => {
+        this.$super(BaseChartMixin).renderChart().then(() => {
+          this.$nextTick(() => {
+            if (this.computedOptions.useImprovedLabels) {
+              this.chart.redraw() // fix label position
+            }
+            return resolve()
+          })
+        })
+      })
+    },
+    getRadius () {
+      const container = document.querySelector(`#chart-${this._uid}`)
+      if (!container) {
+        return 0
+      }
+      let suggestedRadius = container.clientWidth
+      let { minRadius, maxRadius } = this.computedOptions
+      return constrain(suggestedRadius, minRadius, maxRadius)
+    }
+  }
+
+}
+</script>
+
+<style>
+.dc-chart.dc-pie-chart .pie-slice:hover {
+  fill-opacity: 0.8;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,12 @@
       :options="rowChartOptions"
       :dimension-constructor="d => `${d.user_id}.${d.first_name} ${d.last_name}`"
       reducer="hours"
-      :group-options="{ orderNatural: true }"
+    />
+    <dc-pie-chart
+      :ndx="ndx"
+      :options="pieChartOptions"
+      dimension-constructor="team"
+      reducer="hours"
     />
   </div>
 </template>
@@ -14,10 +19,14 @@
 import DcRowChart from '../components/dc-row-chart.vue'
 import sampleData from '../sample-data.js'
 import crossfilter from 'crossfilter2'
+import DcPieChart from '../components/dc-pie-chart.vue'
 
 export default {
   name: 'Home',
-  components: { DcRowChart},
+  components: {
+    DcRowChart,
+    DcPieChart
+  },
   data () {
     return {
       ndx: null
@@ -31,7 +40,15 @@ export default {
       return {
          // label is going to show their name (dimension key part)
          // if they had a . in their name keep it I guess
-        labelAccessor: d => d.key.split('.').slice(1).join('.')
+        label: d => d.key.split('.').slice(1).join('.')
+      }
+    },
+    pieChartOptions () {
+      return {
+        title: d =>`${d.key}: ${d.value.toFixed(2)} hours`,
+        slicesCap: 6,
+        maxRadius: 300,
+        label: d => `${d.key};|;${d.value.toFixed(0)}`
       }
     }
   }
